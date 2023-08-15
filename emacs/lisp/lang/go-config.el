3a003b839409f13c08f930535f9bd337174f09cb
Eugeny Volobuev <qulert@gmail.com>
2023-08-16 09:50:54 +1000

    update go-lang config

--- emacs/lisp/lang/go-config.el
+++ emacs/lisp/lang/go-config.el
@@ -3,8 +3,15 @@
 ;;; Code:
 (use-package go-mode
   :mode ("\\.go\\'")
-  :hook ((go-mode go-ts-mode) . j2/project-eglot-ensure)
+  :hook (((go-mode go-ts-mode) . j2/project-eglot-ensure)
+         ((go-mode go-ts-mode) . j2/go-mode-hook))
   :config (setenv "GOPATH" (expand-file-name "~/Documents/dev/golang/go")))
+
+(defvar go-tab-width 4)
+
+(defun j2/go-mode-hook ()
+  (setq tab-width go-tab-width
+        go-ts-mode-indent-offset go-tab-width))
 
 (provide 'go-config)
 ;;; go-config.el ends here
