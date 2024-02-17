5c51ad4ade04933a7c9cfa90ac392bbea9f9ccdd
Eugeny Volobuev <qulert@gmail.com>
2024-02-17 22:18:58 +0800

    [emacs] sync

--- emacs/lisp/lang/cl-config.el
+++ emacs/lisp/lang/cl-config.el
@@ -4,24 +4,12 @@
 (use-package sly
   :defer t
   :custom
-  ;; (inferior-lisp-program "sbcl")
-  (inferior-lisp-program "ccl64")
+  (inferior-lisp-program "sbcl")
+  ;; (inferior-lisp-program "ccl64")
   ;; (inferior-lisp-program "ros -Q run")
   :config
   (sly-setup)
   (sly-symbol-completion-mode -1))
-
-(use-package sly-asdf
-  :after sly
-  :defer t)
-
-(use-package sly-quicklisp
-  :after sly
-  :defer t)
-
-(use-package sly-repl-ansi-color
-  :after sly
-  :defer t)
 
 (provide 'cl-config)
 ;;; cl-config.el ends here
