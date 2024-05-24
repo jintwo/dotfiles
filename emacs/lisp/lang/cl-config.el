b03154602eacd7ab251a47535a2d5c3ee9e518c6
Eugeny Volobuev <qulert@gmail.com>
2024-05-24 18:22:43 +0800

    sync

--- emacs/lisp/lang/cl-config.el
+++ emacs/lisp/lang/cl-config.el
@@ -4,9 +4,9 @@
 (use-package sly
   :defer t
   :custom
-  (inferior-lisp-program "ros -Q run")
+  (inferior-lisp-program "ros -Q dynamic-space-size=4096 run")
   ;; (inferior-lisp-program "ccl64")
-  ;; (inferior-lisp-program "ros -Q run")
+  ;; (inferior-lisp-program "abcl")
   :config
   (sly-setup)
   (sly-symbol-completion-mode -1))
--- emacs/lisp/lang/scheme-config.el
+++ emacs/lisp/lang/scheme-config.el
@@ -5,12 +5,24 @@
   :defer t)
 
 (use-package geiser-guile
+  :after geiser
   :defer t)
 
-(use-package racket-mode
-  :defer t
-  :init (require 'racket-xp)
-  :hook (racket-mode . racket-xp-mode))
+(use-package geiser-racket
+  :after geiser
+  :defer t)
+
+(when (featurep 'geiser)
+  (defun geiser-repl-switch-to-current-module ()
+    "Switch to running Geiser REPL and try to enter a module associated with current buffer."
+    (interactive)
+    (geiser-repl-switch-to-module (buffer-file-name))))
+
+;; ????
+;; (use-package racket-mode
+;;   :defer t
+;;   :init (require 'racket-xp)
+;;   :hook (racket-mode . racket-xp-mode))
 
 (use-package sicp
   :defer t)
