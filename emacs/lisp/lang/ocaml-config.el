b2a0a7c65298f8e407bcf4f13179916fd8563839
Eugeny Volobuev <qulert@gmail.com>
2025-10-09 09:14:39 +0800

    [emacs] switch to ocamllsp

--- emacs/lisp/lang/ocaml-config.el
+++ emacs/lisp/lang/ocaml-config.el
@@ -1,35 +1,22 @@
 ;;; ocaml-config.el --- ocaml config
 ;;; Commentary:
 ;;; Code:
-;;; TODO: consider moving to simple (probably) caml-mode
 (use-package tuareg
+  :ensure t
   :mode (((rx ".(mll|ml[ip]?|eliomi?)" eos) . tuareg-mode)
          ((rx ".mly" eos) . tuareg-menhir-mode))
   :hook (tuareg-mode . j2/project-eglot-ensure))
 
-(use-package merlin
-  :defer t
-  :hook ((tuareg-mode caml-mode) . merlin-mode)
-  :config (setq merlin-error-after-save nil))
-
-(let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
-  (when (and opam-share (file-directory-p opam-share))
-    ;; Register Merlin
-    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
-    (autoload 'merlin-mode "merlin" nil t nil)
-    ;; Automatically start it in OCaml buffers
-    (add-hook 'tuareg-mode-hook 'merlin-mode t)
-    (add-hook 'caml-mode-hook 'merlin-mode t)
-    ;; Use opam switch to lookup ocamlmerlin binary
-    (setq merlin-command 'opam)
-    ;; To easily change opam switches within a given Emacs session, you can
-    ;; install the minor mode https://github.com/ProofGeneral/opam-switch-mode
-    ;; and use one of its "OPSW" menus.
-    ))
-
-(put 'tuareg-mode 'eglot-language-id "ocaml")
-(put 'tuareg-opam-mode 'eglot-language-id "ocaml")
-(put 'merlin-mode 'eglot-language-id "ocaml")
+(use-package ocaml-eglot
+  :ensure t
+  :after tuareg
+  :hook
+  (tuareg-mode . ocaml-eglot)
+  (ocaml-eglot . eglot-ensure)
+  (ocaml-eglot . (lambda ()
+                   (add-hook #'before-save-hook #'eglot-format nil t)))
+  :config
+  (setq ocaml-eglot-syntax-checker 'flymake))
 
 (use-package opam-switch-mode
   :ensure t
@@ -45,6 +32,5 @@
   :config
   (setq utop-command "opam exec -- dune utop . -- -emacs"))
 
-;; TODO: try ocaml-ts-mode
 (provide 'ocaml-config)
 ;;; ocaml-config.el ends here
