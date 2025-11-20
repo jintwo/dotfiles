64e7f08cde8d149fe61b68dae2b025a3f8821efd
Eugeny Volobuev <qulert@gmail.com>
2025-11-20 18:30:50 +0800

    [hs] fix

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,13 +8,11 @@ hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reload_config):start()
 hs.alert.show('Config loaded')
 
 local letters = {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'}
-local poor_man_ace_window = hs.hotkey.modal.new({'alt', 'ctrl'}, 'o')
 local canvases = {}
-local window_letter_map = {}
+local windows = {}
 
-local function prepare_window_canvas(window, letter)
-    local top_left = window:topLeft()
-    local c = canvas.new{x = top_left.x + 10, y = top_left.y, h = 50, w = 50}
+local function prepare_window_canvas(x, y, letter)
+    local c = canvas.new{x = x + 10, y = y, h = 50, w = 50}
     c[1] = {
         frame = { h = 50, w = 50, x = 0, y = 0 },
         textSize = 50,
@@ -25,50 +23,55 @@ local function prepare_window_canvas(window, letter)
     return c
 end
 
-local function fill_window_letter_map()
+local function prepare_state()
     local index = 1
+    local canvases, windows = {}, {}
     local windows = hs.window.visibleWindows()
     for _, window in ipairs(windows) do
         local title = window:title()
         if string.len(title) > 0 then
             local letter = letters[index]
-            canvases[letter] = prepare_window_canvas(window, letter)
-            window_letter_map[letter] = window
+            local top_left = window:topLeft()
+            local x = top_left.x
+            local y = top_left.y
+            canvases[letter] = prepare_window_canvas(x, y, letter)
+            windows[letter] = window
             index = index + 1
         end
     end
+    return canvases, windows
 end
 
-local function focus_window(letter)
+local function focus_window(letter, modal)
     return function()
-        window_letter_map[letter]:focus()
-        canvases[letter]:hide()
-        poor_man_ace_window:exit()
+        local w = windows[letter]
+        if w then
+            w:focus()
+        end
+        -- remove letters
+        for _, letter in ipairs(letters) do
+            local c = canvases[letter]
+            if c then
+                c:hide()
+            end
+        end
+        modal:exit()
     end
 end
 
-function poor_man_ace_window:entered()
-    canvases = {}
-    window_letter_map = {}
+local poor_man_ace_window = hs.hotkey.modal.new({'alt', 'ctrl'}, 'o')
+poor_man_ace_window:bind('ctrl', 'g', '', function() poor_man_ace_window:exit() end)
+for _, letter in ipairs(letters) do
+    poor_man_ace_window:bind('', letter, nil, focus_window(letter, poor_man_ace_window))
+end
 
-    fill_window_letter_map()
+function poor_man_ace_window:entered()
+    canvases, windows = prepare_state()
+    -- draw letters
     for _, letter in ipairs(letters) do
-        poor_man_ace_window:bind('', letter, nil, focus_window(letter))
         local c = canvases[letter]
         if c then
             c:show()
         end
     end
 end
-
-function poor_man_ace_window:exited()
-    for _, letter in ipairs(letters) do
-        local c = canvases[letter]
-        if c then
-            c:hide()
-        end
-    end
-end
-
-poor_man_ace_window:bind('ctrl', 'g', '', function() poor_man_ace_window:exit() end)
-poor_man_ace_window:exit()
